6d1422d38652fe973328b49960466d1a0f626676
Eloilson <eloilson.ageedu@gmail.com>
2022-07-03 22:57:08 -0300

    feat: add authentication, authorization and test scenarios in groups

--- start/bouncer.ts
+++ start/bouncer.ts
@@ -8,6 +8,7 @@ import User from 'App/Models/User'
  */
 
 import Bouncer from '@ioc:Adonis/Addons/Bouncer'
+import Group from 'App/Models/Group'
 
 /*
 |--------------------------------------------------------------------------
@@ -40,7 +41,12 @@ export const { actions } = Bouncer.define('updateUser', (user: User, updatedUser
   .define('rejectGroupRequest', (user: User, groupRequest: GroupRequest) => {
     return user.id === groupRequest.group.master
   })
-
+  .define('updateGroup', (user: User, group: Group) => {
+    return user.id === group.master
+  })
+  .define('deleteGroup', (user: User, group: Group) => {
+    return user.id === group.master
+  })
 /*
 |--------------------------------------------------------------------------
 | Bouncer Policies
--- start/routes.ts
+++ start/routes.ts
@@ -28,6 +28,11 @@ Route.post('/users', 'UsersController.store')
 Route.put('/users/:id', 'UsersController.update').middleware('auth')
 
 Route.post('/groups', 'GroupsController.store').middleware('auth')
+Route.patch('/groups/:id', 'GroupsController.update').middleware('auth')
+Route.delete('/groups/:id', 'GroupsController.destroy').middleware('auth')
+Route.delete('/groups/:groupId/players/:playerId', 'GroupsController.removePlayer').middleware(
+  'auth'
+)
 
 Route.get('/groups/:groupId/requests', 'GroupRequestsController.index').middleware('auth')
 Route.post('/groups/:groupId/requests', 'GroupRequestsController.store').middleware('auth')
